24cf9b22b2b2d051beec08b3ec6409d258199d1b
spencer-mcguire <smcguire1826@gmail.com>
2020-01-17 17:44:25 -0600

    initial commit

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/dbConfig.js');
+const db = require("./data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.get("/api/accounts", (req, res) => {
+  db("accounts")
+    .then(accounts => {
+      res.json(accounts);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to get users" });
+    });
+});
 
 module.exports = server;
